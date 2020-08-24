4680dc323c5c43aac15de97a93c6481e8e5f8751
OL-Loostrom <pauloo2941@students.ecpi.edu>
2020-08-23 21:55:00 -0400

    Updates

---
+++ precache-manifest.6ccd23d309e5b22c85eed38326d28ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c69ab69a72b70f2bc5ffd463610ad8",
+    "url": "/CIS453L-CourseProject/index.html"
+  },
+  {
+    "revision": "39761acff554baf069a2",
+    "url": "/CIS453L-CourseProject/static/css/main.9bbb3903.chunk.css"
+  },
+  {
+    "revision": "5d588c431d6ee738dada",
+    "url": "/CIS453L-CourseProject/static/js/2.ccbb68ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CIS453L-CourseProject/static/js/2.ccbb68ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39761acff554baf069a2",
+    "url": "/CIS453L-CourseProject/static/js/main.9c33cf44.chunk.js"
+  },
+  {
+    "revision": "d3b3731ebdb68ff71f4b",
+    "url": "/CIS453L-CourseProject/static/js/runtime-main.806a6741.js"
+  }
+]);
